f217e1290decd02ccba6dffbc5bc08ea90b662a0
Godwin <godwinaugustine118@gmail.com>
2025-12-05 08:37:38 +0100

    added my portfolio folder

---
+++ portfolio/js/projectspage.js
@@ -0,0 +1,26 @@
+export const projectsData =  [
+  {
+    image: 'images/quiz.jpg',
+    title: 'Quize-App',
+    tools: 'HTML, CSS, JS.',
+    link: 'https://quiz-test-navy.vercel.app/'
+  },
+  {
+    image: 'images/cgpa.jpg',
+    title: 'CGPA-Calculator',
+    tools: 'HTML, CSS, JS.',
+    link: 'https://cgpa-cal-grades.vercel.app/'
+  },
+  {
+    image: 'images/project3.png',
+    title: 'Lukas-Store (E-Commerce)',
+    tools: 'HTML, CSS, JS.',
+    link: '/'
+  },
+  {
+    image: 'images/timber.jpg.png',
+    title: 'TimberLand',
+    tools: 'HTML, CSS, JS.',
+    link: 'https://godwina5751.github.io/TimberLand.git.com/'
+  }
+]
